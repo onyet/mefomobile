722ce3d2d3ba93a19a69e984cc437328fafc0c7b
Dian Mukti Wibowo <dian.wibowo@clickoding.id>
2019-02-26 21:56:33 +0700

    Menambahkan fungsi dasar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items: any = [];
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  doRefresh(refresher) {
+
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+    
+      console.log('Async operation has ended');
+    
+      refresher.complete();
+    
+    }, 2000);
+  
+  } // end doRefresh
+
+  doInfinite(infiniteScroll): Promise<any> {
+
+    console.log('Begin async operation');
+
+    return new Promise((resolve) => {
+    
+      setTimeout(() => {
+    
+        for (var i = 0; i < 30; i++) {
+    
+          this.items.push( this.items.length );
+    
+        }
+
+        console.log('Async operation has ended');
+    
+        resolve();
+
+        infiniteScroll.complete();
+    
+      }, 500);
+    
+    });
+
+  } // end doInfinite
 
 }
